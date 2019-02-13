f6bdd6da0c3e03ab3158796a7ecc8c970ef26e9c
stoemason <vinsteimes@gmail.com>
2019-02-13 16:21:10 +0100

    add rudimentary recursion (rotations are wrong)

--- portal.gd
+++ portal.gd
@@ -4,27 +4,32 @@ export(NodePath) var portal_exit_path
 export(int) var max_recursion = 1
 var portal_exit
 
-onready var portal_meshes = $portal_meshes.get_children()
+var portal_meshes = []
 
 func _ready():
 	portal_exit = get_node(portal_exit_path)
-	for i in range(max_recursion):
+	var meshes = $portal_meshes.get_children()
+	for i in range(min(max_recursion, meshes.size()-1)):
+		portal_meshes.append(meshes[i])
+		portal_meshes[i].material_override = portal_meshes[i].material_override.duplicate()
 		portal_meshes[i].material_override.set_shader_param("viewport_texture", portal_exit.get_viewport_texture(i))
 
 func _on_portal_activation_body_entered(body):
 	body.connect("moved", self, "_on_player_moved")
 	show()
-#	print("body entered")
 
 func _on_portal_activation_body_exited(body):
 	body.disconnect("moved", self, "_on_player_moved")
 	hide()
-#	print("body left")
 
 func _on_player_moved(player):
-	portal_exit.set_camera_position(to_local(player.camera.global_transform.origin))
-	var localised_rot = player.camera.global_transform.basis.orthonormalized().get_euler() - global_transform.basis.orthonormalized().get_euler()
-	portal_exit.set_camera_rotation(localised_rot)
+	var previous_camera_location = player.camera.global_transform.origin
+	var previous_camera_basis = player.camera.global_transform.basis
+	var own_rot = global_transform.basis.orthonormalized().get_euler()
+	for i in range(max_recursion):
+		previous_camera_location = portal_exit.set_camera_position(to_local(previous_camera_location), i)
+		var localised_rot = previous_camera_basis.orthonormalized().get_euler() - own_rot
+		previous_camera_basis = portal_exit.set_camera_rotation(localised_rot)
 
 func _on_portal_entry_body_entered(body):
 	if body.get_movement_direction().dot(global_transform.basis.z) < 0:		
--- portal_exit.gd
+++ portal_exit.gd
@@ -1,15 +1,23 @@
 extends Spatial
 
+var viewports = []
+var cameras = []
+
+func _ready():
+	viewports = get_children()
+	for vp in viewports:
+		vp.size = get_viewport().size
+		cameras.append(vp.get_node("Camera"))
 
 func get_viewport_texture(recursion_level = 0):
-	$Viewport.size = get_viewport().size
-	return $Viewport.get_texture()
+#	$Viewport.size = get_viewport().size
+	return viewports[recursion_level].get_texture()
 	
-func set_camera_position(relative_pos):
-	$Viewport/Camera.global_transform.origin = to_global(relative_pos)
-	return $Viewport/Camera.global_transform.origin
+func set_camera_position(relative_pos, recursion_level = 0):
+	cameras[recursion_level].global_transform.origin = to_global(relative_pos)
+	return cameras[recursion_level].global_transform.origin
 	
-func set_camera_rotation(source_rot):
+func set_camera_rotation(source_rot, recursion_level = 0):
 	var target_rot = global_transform.basis.orthonormalized().get_euler() + source_rot
-	$Viewport/Camera.global_transform.basis = Basis(target_rot)
-	return $Viewport/Camera.global_transform.basis
+	cameras[recursion_level].global_transform.basis = Basis(target_rot)
+	return cameras[recursion_level].global_transform.basis
